52939cc7211c093bb2b0dcbb2f4cde75230204a4
Parvathy36 <159471840+Parvathy36@users.noreply.github.com>
2025-04-09 21:35:45 +0530

    Update and rename Item.js to Task.js

--- models/Item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ItemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, default: 0 }, 
-});
-
-module.exports = mongoose.model("Item", ItemSchema);
