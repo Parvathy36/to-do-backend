d10fbbb1d9ac6032019adf3dc6993e9f1a62fd5d
Parvathy36 <159471840+Parvathy36@users.noreply.github.com>
2025-04-10 10:27:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ app.use(cors());
 app.use(express.json()); 
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://parvathys2026:Parvathy33@cluster0.cvwrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("Connected to MongoDB"))
+mongoose.connect("mongodb+srv://parvathys2026:Parvathy33@cluster0.cvwrw.mongodb.net/todo-app?retryWrites=true&w=majority&appName=Cluster0", 
+  { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("Database connection error:", err));
 
 // Import routes
